76edc29ed6fb443aeeb4b8bdd2080086cba0d680
Deepayan Sarkar <deepayan.sarkar@gmail.com>
2024-07-13 15:57:34 +0530

    regenerate index with fresh update from CRAN

--- generate-index.R
+++ generate-index.R
@@ -40,6 +40,8 @@ cat(file = INDEX_FILE, append = FALSE, sep = "\n",
     "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH' crossorigin='anonymous'>",
     "</head><body><div class='container-fluid'>",
     "<h1>R Package Manuals</h1>",
+    "<h2>Index generated on: ", as.character(Sys.Date()), "</h2>",
+    
     "<hr/>",
     "<table class='table table-striped'>",
     "<tr><th>Package</th><th>Version</th><th>Title</th><th>Maintainer</th></tr>")
